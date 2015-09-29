b940c8498b7b53825f78b9a7f3fb603c07e07f27
harith bin mohd salleh <harithsalleh@hariths-MacBook-Air.local>
2015-09-29 15:59:44 +0800

    first commit

--- todo.rb
+++ todo.rb
@@ -1,3 +1,9 @@
 require_relative 'config/application'
+require_relative 'app/models/task'
+require 'byebug'
 
 puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+byebug
+
+puts "hello"
