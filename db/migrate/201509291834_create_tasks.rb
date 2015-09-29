181d0c3ca6955f1e3fcdccac1bd54dbd5a757a4f
harith bin mohd salleh <harithsalleh@hariths-MacBook-Air.local>
2015-09-29 19:19:06 +0800

    linked model to database tasks table

--- db/migrate/201509291834_create_tasks.rb
+++ db/migrate/201509291834_create_tasks.rb
@@ -3,9 +3,8 @@ require_relative '../../config/application'
 class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
-		t.string :task
-		t.integer :status, limit: 1, default: 0
-		t.timestamps null: false
+		t.string :name
+		t.integer :complete
     end
   end
 end
--- todo.rb
+++ todo.rb
@@ -2,8 +2,6 @@ require_relative 'config/application'
 require_relative 'app/models/task'
 require 'byebug'
 
+byebug 
+
 puts "Put your application code in #{File.expand_path(__FILE__)}"
-
-byebug
-
-puts "hello"
